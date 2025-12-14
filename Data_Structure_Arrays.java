06ef86665da99dc40ea75172013746cff009f3a6
Alf2502 <lr.aaborde@mmdc.mcl.edu.ph>
2025-12-15 00:11:24 +0800

    Additional files: Data_Structure_Arrays.java

---
+++ Data_Structure_Arrays.java
@@ -0,0 +1,17 @@
+public class Data_Structure_Arrays {
+    public static void main(String[] args) {
+        // Declare and initialize an array
+        int[] employeeIDs = {101, 102, 103, 104, 105};
+        
+        // Print array elements
+        System.out.println("Array elements:");
+        for (int emp_id : employeeIDs) {
+            System.out.println(emp_id);
+        }
+        
+        System.out.println();
+        System.out.println("Accessing specific element:");
+        // Access specific element
+        System.out.println(employeeIDs[2]);
+    }
+}
